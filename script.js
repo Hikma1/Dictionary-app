c6d000ef901a173e9d1317f4634e89f3af485468
Hikma1 <hikma0227@gmail.com>
2025-12-23 16:12:15 +0300

    aothercss

--- script.js
+++ script.js
@@ -15,6 +15,45 @@ async function fetchandCreateCard(){
 
 
     console.log(data)
+
+
+
+    let partOfSpeechArray=[]
+
+for (let i=0; i<data[0].meanings.length-1;i++){
+    partOfSpeechArray.push(data.meanings[i].partOfSpeech)
+
+}
+
+    dictionary.innerHTML=`
+     <div class="card">
+                <!--Word-->
+                <div class="property">
+                    <span>Word</span>
+                    <span>${data.word}</span>
+                </div>
+                <!--Phonetic-->
+                 <div class="property">
+                    <span>Phonetic</span>
+                    <span>${data.phonetic}</span>
+                </div>
+                <!--Definition-->
+                 <div class="property">
+                    <span>Definition</span>
+                    <span>${data.meanings[0].definitions[0].definition}</span>
+                </div>
+                <!--Example-->
+                 <div class="property">
+                    <span>Example</span>
+                    <span>${data.meanings[0].definitions[0].example || 'N/A'}</span>
+                </div>
+                <!--Parts of Speech-->
+                 <div class="property">
+                    <span>Parts of Speech</span>
+                    <span>${partOfSpeechArray.join(', ')}</span>
+                </div>
+            </div>'
+
 }
 
 
